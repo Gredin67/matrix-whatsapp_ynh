d23a43326ae7be68ee04e1242cec53f2bfd831aa
Gredin67 <35761345+Gredin67@users.noreply.github.com>
2020-04-13 13:28:17 +0200

    removed golang 1.7 dependency
    golang 1.11 installed by helper extra_

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="golang postgresql postgresql-client"
+pkg_dependencies="postgresql postgresql-client"
 
 #=================================================
 # PERSONAL HELPERS
